1696a6327dce5a8d0a240e8c7df97fcd4ea1c43d
kislyakovm <maksim@druble.ru>
2022-11-03 06:27:46 +0300

    stage 3/5, add game that will predict the age of a user

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -10,8 +10,18 @@ public class SimpleBot {
         System.out.println("I was created in 2018.");
         System.out.println("Please, remind me your name.");
 
-        String userName = scanner.next();
+        String name = scanner.nextLine();
 
-        System.out.printf("What a great name you have, %s!", userName);
+        System.out.println("What a great name you have, " + name + "!");
+        System.out.println("Let me guess your age.");
+        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+
+        int remainder3 = scanner.nextInt();
+        int remainder5 = scanner.nextInt();
+        int remainder7 = scanner.nextInt();
+
+        int yourAge = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
+
+        System.out.printf("Your age is %d; that's a good time to start programming!", yourAge);
     }
 }
